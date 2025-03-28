ec3bf08423af8969149b2cbb2c63b7e88174e7aa
noradech <prabnrd@gmail.com>
2025-03-29 02:56:00 +0700

    ส่งงาน

---
+++ Lab09_Ex02/Program.cs
@@ -0,0 +1,13 @@
+﻿﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Person.name="Noradech";
+System.Console.WriteLine(Person.Getname());
+
+static class Person
+{
+     static public  string? name;
+     static public string Getname()
+    {
+        return $"Hello from {name}"; 
+    }
+}
---
+++ Lab09_Ex03/Program.cs
@@ -0,0 +1,25 @@
+﻿﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Person.name = "Noradech"; 
+System.Console.WriteLine(Person.Getname());
+var student = new Student();
+Student.name = "Noradech";
+System.Console.WriteLine(student.Getname());
+
+static class Person
+{
+    static public string? name;
+    static public string Getname()
+    {
+        return $"Hello from {name}"; 
+    }
+}
+
+class Student
+{
+    public static string? name; 
+    public string Getname()
+    {
+        return $"Hello from {name}"; 
+    }
+}
